51d337a1a12945a6e880e66aa0b28d5169e7b170
Ziemniak <potatoosalad@gmail.com>
2020-04-01 10:57:22 +0200

    Some little ortografic changes for linux users

--- project/main.cpp
+++ project/main.cpp
@@ -21,14 +21,14 @@ int main()
 {
     std::cout << "Henlo!" << std::endl;
 
-    Entity e1(0,5), e2(1,5), e3(3,5);
+    Entity e1(0, 5), e2(1, 5), e3(3, 5);
 
     class MockComponent : public Component
     {
         public:
         int a; int b;
-        MockComponent(int x, int y) { a=x; b=y;}
-    } mockComponent(2,2), mockComponent2(21,37);
+        MockComponent(int x, int y) { a = x; b = y;}
+    } mockComponent(2, 2), mockComponent2(21, 37);
 
     class MockInvalidComponent : public Component
     {
@@ -54,7 +54,7 @@ int main()
         virtual void Update()
         {
             int a = ptr->a, b = ptr->b;
-            std::cout << a << "+" << b << "=" << a+b << std::endl;
+            std::cout << a << "+" << b << "=" << a + b << std::endl;
         }
     } mockSystem;
 
@@ -63,7 +63,7 @@ int main()
     Message m1(Event::KEY_PRESSED_A);
     Message m2(Event::KEY_PRESSED_A, 123);
     Message m3(Event::KEY_PRESSED_A, 0.2);
-    Message m4(Event::KEY_PRESSED_A, float2(21.0f,37.0f));
+    Message m4(Event::KEY_PRESSED_A, float2(21.0f, 37.0f));
     std::cout << m2.getValue<int>() << ", " << m3.getValue<double>() << std::endl;
     std::cout << m4.getValue<float2>().first << ", " << m4.getValue<float2>().second << std::endl;
 
