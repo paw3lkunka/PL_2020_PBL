63258264fc15d551afd91c3d59e879c212341139
Aandrzejmi <andrzej.mi@op.pl>
2020-04-21 23:13:58 +0000

    Some cleanup

---
+++ project/GameSystems/Systems/src/CollisionDetectionSystem.cpp
@@ -0,0 +1,19 @@
+#include "CollisionDetectionSystem.hpp"
+
+#include "Entity.hpp"
+#include "ComponentType.inl"
+#include "Components.inl"
+
+void CollisionDetectionSystem::start()
+{
+
+}     
+
+bool CollisionDetectionSystem::assertEntity(Entity* entity)
+{
+}
+
+void CollisionDetectionSystem::fixedUpdate()
+{
+
+}
