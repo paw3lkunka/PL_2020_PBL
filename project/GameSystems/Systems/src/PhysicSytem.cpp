93bdffe49de37c831158e53b6b1c85951dc599aa
AandrzejMi <andrzej.mi@op.pl>
2020-05-18 17:44:31 +0200

    Working drag THIS IS A GREAT DAY

--- project/GameSystems/Systems/src/PhysicSytem.cpp
+++ project/GameSystems/Systems/src/PhysicSytem.cpp
@@ -28,41 +28,19 @@ void PhysicSystem::fixedUpdate()
         torque += glm::cross(impuse.point, impuse.force);
     }
 
-    glm::vec3 dragForce = -0.5f * rBodyPtr->drag * rBodyPtr->velocity * glm::abs(rBodyPtr->velocity);
-    force += dragForce;
-
     if (!rBodyPtr->ignoreGravity)
     {
         force += -rBodyPtr->mass * G_CONST;
     }
 
     glm::vec3 acceleration = force / rBodyPtr->mass;
-    rBodyPtr->velocity += acceleration * Core::FIXED_TIME_STEP_F;    
-
-    glm::vec3 dragTorque = -0.5f * rBodyPtr->angularDrag * rBodyPtr->angularVelocity * glm::abs(rBodyPtr->angularDrag);
-
-    std::cout << "B------------------------------\n" 
-            << "AngDrag     " << rBodyPtr->angularDrag << '\n'
-            << "AngVelocity " << glm::to_string(rBodyPtr->angularVelocity) << '\n'
-            << "DragTorque  " << glm::to_string(dragTorque) << '\n'
-            << "E------------------------------\n" << std::endl;
-
-
-    if(glm::zero<glm::vec3>() != dragTorque)
-    {
-    std::cout << "Torque: " << glm::to_string(torque) << "\nDrag torque: " << glm::to_string(dragTorque) << std::endl;
-
-    }
-
-    if(std::isnan(dragTorque.x))
-    {
-        exit(666);
-    }
-
-    torque += dragTorque;
+    rBodyPtr->velocity += acceleration * Core::FIXED_TIME_STEP_F;
 
     glm::vec3 angularAcceleration = rBodyPtr->momentOfInertia * torque;
     rBodyPtr->angularVelocity += angularAcceleration * Core::FIXED_TIME_STEP_F;
+
+    rBodyPtr->velocity *= glm::exp(-rBodyPtr->drag * Core::FIXED_TIME_STEP_F);
+    rBodyPtr->angularVelocity *= glm::exp(-rBodyPtr->angularDrag * Core::FIXED_TIME_STEP_F);
 
     //TODO VELOCITY W AUDIO
 
