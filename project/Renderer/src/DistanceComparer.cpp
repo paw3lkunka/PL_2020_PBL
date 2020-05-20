c3fd19c4e215fa121543a4c8a9305ae4f6274c60
AandrzejMi <andrzej.mi@op.pl>
2020-05-20 13:50:23 +0200

    Sharder fixed, sorting tested

--- project/Renderer/src/DistanceComparer.cpp
+++ project/Renderer/src/DistanceComparer.cpp
@@ -11,5 +11,5 @@ bool DistanceComparer::operator() (NormalPacket* packet1, NormalPacket* packet2)
     glm::vec3 pos1 = packet1->modelMatrix[3];
     glm::vec3 pos2 = packet2->modelMatrix[3];
 
-    return glm::length(cameraPos - pos1) < glm::length(cameraPos - pos2);
+    return glm::length(cameraPos - pos1) > glm::length(cameraPos - pos2);
 }
--- resources/Shaders/UnlitBillboardInstanced/UnlitBillboardInstanced.vert
+++ resources/Shaders/UnlitBillboardInstanced/UnlitBillboardInstanced.vert
@@ -11,11 +11,10 @@ layout (std140, binding = 0) uniform Camera
     vec3 viewPos;
 };
 
-uniform mat4 MVP;
 out vec2 uv;
 
 void main()
 {
-    gl_Position = MVP * vec4(position, 1.0);
+    gl_Position = projection * view * instanceModel * vec4(position, 1.0);
     uv = texcoord;
 }
