dd05a548cb7a41e9b065b52935460d3c40416a6f
AandrzejMi <andrzej.mi@op.pl>
2020-05-20 14:02:19 +0200

    DistanceComparer documentation

--- project/Renderer/include/DistanceComparer.hpp
+++ project/Renderer/include/DistanceComparer.hpp
@@ -4,12 +4,27 @@
 #include "NormalPacket.inl"
 #include <glm/glm.hpp>
 
+/**
+ * @brief Comparer used to sorting transparent queue.
+ */
 class DistanceComparer
 {
     public:
+        /**
+         * @brief Construct a new Distance Comparer object.
+         * 
+         * @param cameraPos global position of camera.
+         */
         DistanceComparer(glm::vec3 cameraPos);
         virtual ~DistanceComparer() = default;
 
+        /**
+         * @brief Compares two render packet, by distance ot their meshes to camera.
+         * 
+         * @param packet1 Renderer packet to compare.
+         * @param packet2 Renderer packet to compare.
+         * @return If packet1 is further than packet2.
+         */
         bool operator() (NormalPacket* packet1, NormalPacket* packet2);
     protected:
     private:
