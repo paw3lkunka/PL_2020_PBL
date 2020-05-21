aa900fae1f0e2ca3586844e3654b8ff0782d8d5f
AandrzejMi <andrzej.mi@op.pl>
2020-05-21 10:56:08 +0200

    Rework of collisions, Code of boxes ready, UNTESTED

--- project/GameSystems/Systems/include/CollisionSystem.ipp
+++ project/GameSystems/Systems/include/CollisionSystem.ipp
@@ -1,6 +1,7 @@
 #ifndef COLLISION_DETECTION_SYSTEM_IPP
 #define COLLISION_DETECTION_SYSTEM_IPP
 
+#include "CollisionSystemHelperFunctions.hpp"
 #include "CollisionDataStructures.inl"
 #include "Message.inl"
 #include "Core.hpp"
@@ -82,6 +83,32 @@ void CollisionSystem::collisionWith(T1* collider1, T2* collider2, Transform* tra
     }
 }
 
+
+template<class T>
+void CollisionSystem::resolveCollsion(T* collider1, SphereCollider* collider2, Rigidbody* body1, Rigidbody* body2, Transform* transform1, Transform* transform2)
+{
+    glm::vec3& normal = testResult.collisionNormal;
+    glm::vec3 momentum1 = body1->mass * body1->velocity;
+    glm::vec3 momentum2 = body2->mass * body2->velocity;
+
+    body1->velocity = ( momentum1 + momentum2 + body2->mass * glm::e<float>() * ( body2->velocity - body1->velocity) ) / ( body1->mass + body2->mass );
+
+    body1->angularVelocity = glm::cross(normal, glm::cross(normal, body1->angularVelocity) + glm::vec3(0.0f, body2->velocity.y, body2->velocity.z) );
+}
+
+template<class T>
+void CollisionSystem::resolveCollsion(T* collider1, BoxCollider* collider2, Rigidbody* body1, Rigidbody* body2, Transform* transform1, Transform* transform2)
+{
+    glm::vec3 r1 = testResult.collisionCentre - static_cast<glm::vec3>( transform1->localToWorldMatrix * glm::vec4(transform1->getLocalPosition() + collider1->center, 1.0f) );
+    glm::vec3 r2 = testResult.collisionCentre - static_cast<glm::vec3>( transform2->localToWorldMatrix * glm::vec4(transform2->getLocalPosition() + collider2->center, 1.0f) );
+
+    glm::vec3 jImpulse = JImpulse(body1, body2, r1, r2, testResult.collisionNormal);
+
+    body1->velocity = body1->velocity + jImpulse * testResult.collisionNormal / body1->mass;
+    //TODO I^-1 chould be pre computed
+    body1->angularVelocity = body1->velocity + glm::inverse(body1->momentOfInertia) * ( glm::cross(r1, jImpulse) * testResult.collisionNormal);
+}
+
 template<class T1, class T2>
 bool CollisionSystem::SATTest(T1* collider1, T2* collider2, Transform* transform1, Transform* transform2, std::vector<glm::vec3>& axes)
 {
@@ -99,8 +126,8 @@ bool CollisionSystem::SATTest(T1* collider1, T2* collider2, Transform* transform
 
     for (int i = 0; i < tests; i++)
     {        
-        Projection1D proj1 = axisProjection(collider1, transform1, axes[START(i)], axes[END(i)], proj1Buffer + START(i));
-        Projection1D proj2 = axisProjection(collider2, transform2, axes[START(i)], axes[END(i)], proj2Buffer + START(i));
+        Projection1D proj1 = AxisProjection(collider1, transform1, axes[START(i)], axes[END(i)], proj1Buffer + START(i));
+        Projection1D proj2 = AxisProjection(collider2, transform2, axes[START(i)], axes[END(i)], proj2Buffer + START(i));
 
         // 1.start, 2.start, 1.end, 2.end
         if (proj1.start < proj2.start && proj1.end > proj2.start)
@@ -136,15 +163,16 @@ bool CollisionSystem::SATTest(T1* collider1, T2* collider2, Transform* transform
 template<class T>
 glm::vec3 CollisionSystem::collisionNormal(T* collider1, SphereCollider* collider2, Transform* transform1, Transform* transform2)
 {
-    return transform2->localToWorldMatrix * glm::vec4(transform2->getLocalPosition() + collider2->center, 1.0f)
-        - transform2->localToWorldMatrix * glm::vec4(transform2->getLocalPosition() + collider2->center, 1.0f); 
+    glm::vec3 vector = transform2->localToWorldMatrix * glm::vec4(transform2->getLocalPosition() + collider2->center, 1.0f)
+            - transform1->localToWorldMatrix * glm::vec4(transform1->getLocalPosition() + collider1->center, 1.0f); 
+    return glm::normalize(vector);
 }
 
 template<class T>
 glm::vec3 CollisionSystem::collisionNormal(T* collider1, BoxCollider* collider2, Transform* transform1, Transform* transform2)
 {
     glm::vec3 centralVector = transform2->localToWorldMatrix * glm::vec4(transform2->getLocalPosition() + collider2->center, 1.0f)
-        - transform2->localToWorldMatrix * glm::vec4(transform2->getLocalPosition() + collider2->center, 1.0f); 
+        - transform1->localToWorldMatrix * glm::vec4(transform1->getLocalPosition() + collider1->center, 1.0f); 
 
     glm::vec3 potentialNormals[]
     {
@@ -169,7 +197,7 @@ glm::vec3 CollisionSystem::collisionNormal(T* collider1, BoxCollider* collider2,
         }
     }
 
-    return potentialNormals[normalIndex];
+    return glm::normalize(potentialNormals[normalIndex]);
 }
 
 #endif
--- project/GameSystems/Systems/src/CollisionSystem.cpp
+++ project/GameSystems/Systems/src/CollisionSystem.cpp
@@ -44,63 +44,6 @@ void CollisionSystem::fixedUpdate()
     }  
 }
 
-Projection1D CollisionSystem::axisProjection(SphereCollider* sphere, Transform* transform, glm::vec3 axisPoint1, glm::vec3 axisPoint2, glm::vec3 projBuffer[])
-{
-    glm::vec3 centreWS = transform->localToWorldMatrix * glm::vec4(transform->getLocalPosition() + sphere->center, 1.0f);
-    glm::vec3 projCentre = axisProjection(centreWS, axisPoint1, axisPoint2);
-    float centre1D = toLineSpace1D(projCentre, axisPoint1, axisPoint2);
-
-    glm::vec3 projectionEdgeOffset = glm::normalize(axisPoint2 - axisPoint1) * sphere->radius;
-
-    projBuffer[0] = projCentre - projectionEdgeOffset;
-    projBuffer[1] = projCentre + projectionEdgeOffset;
-
-    return {centre1D - sphere->radius, centre1D + sphere->radius};
-}
-
-Projection1D CollisionSystem::axisProjection(BoxCollider* box, Transform* transform, glm::vec3 axisPoint1, glm::vec3 axisPoint2, glm::vec3 projBuffer[])
-{
-    Projection1D result = {INFINITY, -INFINITY};
-
-    for (glm::vec4& vert : box->verts)
-    {
-        glm::vec3 vertWS = transform->localToWorldMatrix * vert;
-
-        glm::vec3 projVert = axisProjection(vertWS, axisPoint1, axisPoint2);
-        float centre1D = toLineSpace1D(projVert, axisPoint1, axisPoint2);
-
-        if (centre1D < result.start)
-        {
-            result.start = centre1D;
-            projBuffer[0] = vertWS;
-        }
-        
-        if (centre1D > result.end)
-        {
-            result.end = centre1D;
-            projBuffer[1] = vertWS;
-        }
-    }
-
-    return result;
-}
-
-glm::vec3 CollisionSystem::axisProjection(glm::vec3 point, glm::vec3 axisPoint1, glm::vec3 axisPoint2)
-{
-    //URL: https://gamedev.stackexchange.com/questions/72528/how-can-i-project-a-3d-point-onto-a-3d-line
-    glm::vec3 AB = axisPoint2 - axisPoint1;
-    glm::vec3 AP = point - axisPoint1;
-
-    return axisPoint1 + glm::dot(AP,AB) / glm::dot(AB,AB) * AB;
-}
-
-float CollisionSystem::toLineSpace1D(glm::vec3 point, glm::vec3 axisPoint1, glm::vec3 axisPoint2)
-{
-    glm::vec3 proj0 = axisProjection(glm::zero<glm::vec3>(), axisPoint1, axisPoint2);
-    glm::vec3 diff = point - proj0;
-    return glm::length(diff) * glm::sign(glm::dot(diff, axisPoint2 - axisPoint1));
-}
-//TODO A może tak przeciążenie funkcji?
 template<>  // Specialization only for this case
 bool CollisionSystem::detectCollsion<SphereCollider, SphereCollider>(SphereCollider* coll1, SphereCollider* coll2, Transform* trans1, Transform* trans2)
 {
@@ -111,15 +54,13 @@ bool CollisionSystem::detectCollsion<SphereCollider, SphereCollider>(SphereColli
 
     return SATTest(coll1, coll2, trans1, trans2, axes);
 }
-//TODO A może tak przeciążenie funkcji?
-template<>
-void CollisionSystem::findSATAxes<SphereCollider>(SphereCollider* collider, Transform* transform, std::vector<glm::vec3>& axes)
+
+void CollisionSystem::findSATAxes(SphereCollider* collider, Transform* transform, std::vector<glm::vec3>& axes)
 {
     return; // Yes, this function should be empty.
 }
-//TODO A może tak przeciążenie funkcji?
-template<>
-void CollisionSystem::findSATAxes<BoxCollider>(BoxCollider* collider, Transform* transform, std::vector<glm::vec3>& axes)
+
+void CollisionSystem::findSATAxes(BoxCollider* collider, Transform* transform, std::vector<glm::vec3>& axes)
 {
     // Local X
     axes.push_back(transform->localToWorldMatrix * (glm::vec4(transform->getLocalPosition(), 0.0f) + collider->verts[0]));
@@ -132,74 +73,4 @@ void CollisionSystem::findSATAxes<BoxCollider>(BoxCollider* collider, Transform*
     // Local Z
     axes.push_back(transform->localToWorldMatrix * (glm::vec4(transform->getLocalPosition(), 0.0f) + collider->verts[0]));
     axes.push_back(transform->localToWorldMatrix * (glm::vec4(transform->getLocalPosition(), 0.0f) + collider->verts[3]));
-}
-
-/*
- * * * * * * * * * * * * * * * * * * * * * * * * * 
- *  Annotation for resolveCollsion()             *
- *  Vectors n1,2,3 explanation:                  *
- *      v - velocity of collider                 *
- *      n1 - vector betwen centre of colliders   *
- *      n2 = v x n1                              *
- *      n3 = n1 x n2                             *
- * * * * * * * * * * * * * * * * * * * * * * * * *
- */
-//TODO A może tak przeciążenie funkcji?
-template<>
-void CollisionSystem::resolveCollsion<SphereCollider,SphereCollider>(SphereCollider* collider1, SphereCollider* collider2, Rigidbody* body1, Rigidbody* body2, Transform* transform1, Transform* transform2)
-{
-    glm::vec3 n1 = transform2->localToWorldMatrix * glm::vec4(transform2->getLocalPosition() + collider2->center, 1.0f)
-    - transform1->localToWorldMatrix * glm::vec4(transform1->getLocalPosition() + collider2->center, 1.0f);
-
-    n1 = glm::normalize(n1);
-
-    body1->velocity =
-    (
-        body1->mass * body1->velocity + body2->mass * body2->velocity
-        + body2->mass * glm::e<float>() * ( body2->velocity - body1->velocity)
-    )
-    / ( body1->mass + body2->mass );
-
-    body1->angularVelocity = 
-    glm::cross
-    (
-        n1,
-        glm::cross(n1, body1->angularVelocity) + glm::vec3(0.0f, body2->velocity.y, body2->velocity.z)
-    );
-
-    std::cout << "n1" <<  glm::to_string(n1) << "\n"
-            << "AngV1:" <<  glm::to_string(body1->angularVelocity) << "\n"
-            << "AngV2: " << glm::to_string(body2->angularVelocity) << std::endl;
-}
-//TODO A może tak przeciążenie funkcji?
-template<>
-void CollisionSystem::resolveCollsion<SphereCollider,BoxCollider>(SphereCollider* collider1, BoxCollider* collider2, Rigidbody* body1, Rigidbody* body2, Transform* transform1, Transform* transform2)
-{
-    //TODO implementation
-}
-//TODO A może tak przeciążenie funkcji?
-template<>
-void CollisionSystem::resolveCollsion<BoxCollider,SphereCollider>(BoxCollider* collider1, SphereCollider* collider2, Rigidbody* body1, Rigidbody* body2, Transform* transform1, Transform* transform2)
-{
-    //TODO implementation
-}
-//TODO A może tak przeciążenie funkcji?
-template<>
-void CollisionSystem::resolveCollsion<BoxCollider,BoxCollider>(BoxCollider* collider1, BoxCollider* collider2,  Rigidbody* body1, Rigidbody* body2, Transform* transform1, Transform* transform2)
-{
-    glm::vec3 n1 = transform2->localToWorldMatrix * glm::vec4(transform2->getLocalPosition() + collider2->center, 1.0f)
-    - transform1->localToWorldMatrix * glm::vec4(transform1->getLocalPosition() + collider2->center, 1.0f);
-
-    //TODO unfinished
-
-    glm::vec3 r1;
-    glm::vec3 r2;
-
-    glm::vec3 jImpulse = JImpulse(body1, body2, r1, r2, testResult.collisionNormal);
-
-    body1->velocity = body1->velocity + jImpulse * testResult.collisionNormal / body1->mass;
-    //TODO I^-1 chould be pre computed
-    body1->angularVelocity = body1->velocity + glm::inverse(body1->momentOfInertia) * ( glm::cross(r1, jImpulse) * testResult.collisionNormal);
-
-
 }
