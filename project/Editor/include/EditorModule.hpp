2e516af0ac666a7e5af07013b1d2f3423efd7725
Ziemniak <potatoosalad@gmail.com>
2020-05-22 16:34:17 +0200

    Initialize editor module

---
+++ project/Editor/include/EditorModule.hpp
@@ -0,0 +1,14 @@
+#ifndef EDITORMODULE_HPP_
+#define EDITORMODULE_HPP_
+
+class EditorModule
+{
+public:
+    EditorModule();
+    ~EditorModule();
+
+protected:
+private:
+};
+
+#endif /* !EDITORMODULE_HPP_ */
