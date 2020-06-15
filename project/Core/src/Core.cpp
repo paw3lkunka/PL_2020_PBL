d7afde99d515ef01cd8cb8f8a1332b7c7c02fc99
Ziemniak <potatoosalad@gmail.com>
2020-06-16 00:22:09 +0200

    Cargo buttons

--- project/Core/src/Core.cpp
+++ project/Core/src/Core.cpp
@@ -126,8 +126,8 @@ int Core::init()
     if (recreateScene)
     {
         // ? -r
-        #include "../../resources/Scenes/main_Menu.icpp"
-        //#include "../../resources/Scenes/selectCargoScene.icpp"
+        //#include "../../resources/Scenes/main_Menu.icpp"
+        #include "../../resources/Scenes/selectCargoScene.icpp"
         //#include "../../resources/Scenes/scene_old.icpp"
         //#include "../../resources/Scenes/testScene.icpp"
         //#include "../../resources/Scenes/newScene.icpp"
