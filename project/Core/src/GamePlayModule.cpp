32b30afde3ad163942718d5c2b6c9f665350f77c
Ziemniak <potatoosalad@gmail.com>
2020-06-18 12:39:29 +0200

    Fixed pause screen + you died screen

--- project/Core/src/GamePlayModule.cpp
+++ project/Core/src/GamePlayModule.cpp
@@ -25,10 +25,23 @@ void GamePlayModule::receiveMessage(Message msg)
         break;
 
         case Event::PAUSE_GAME:
+            Core::instance->gamePaused = !Core::instance->gamePaused;
+            Core::instance->messageBus.sendMessage(Message(Event::AUDIO_SOURCE_PAUSE_ALL_PLAYING));
             pauseGame();
+        break;
+
+        case Event::HP_0:
+            Core::instance->gamePaused = !Core::instance->gamePaused;
+            Core::instance->messageBus.sendMessage(Message(Event::AUDIO_SOURCE_PAUSE_ALL_PLAYING));
+            youDied();
         break;
         
         case Event::LOAD_SCENE:
+            if(Core::instance->gamePaused)
+            {
+                Core::instance->gamePaused = !Core::instance->gamePaused;
+                Core::instance->messageBus.sendMessage(Message(Event::AUDIO_SOURCE_PAUSE_ALL_PLAYING));
+            }
             reloadScene(msg.getValue<const char*>());
         break;
     }
@@ -46,6 +59,7 @@ void GamePlayModule::init()
 
     initLoadingScreen(om);
     initPauseScreen(om);
+    initYouDieScreen(om);
 }
 
 void GamePlayModule::reloadScene(std::string name)
@@ -194,6 +208,7 @@ void GamePlayModule::initPauseScreen(ObjectModule& om)
                 butt->inactiveColor = inactiveColor;
                 butt->highlightedColor = highlightedColor;
                 butt->onClickColor = onClickColor;
+                butt->onClickEvents.push_back(Message(Event::PAUSE_GAME));
                 butt->onClickEvents.push_back(Message(Event::LOAD_SCENE, Scenes::mainMenuScene));
                 entity->addComponent(butt);
         }
@@ -216,13 +231,141 @@ void GamePlayModule::initPauseScreen(ObjectModule& om)
     }   
 }
 
+void GamePlayModule::initYouDieScreen(ObjectModule& om)
+{
+    youDiedScreenEntity = new Entity(-8, 2, "DieScreen");
+    auto bg = om.newTexture("Resources/Sprites/paper.png", info);
+    auto uiRenderer = new UiRenderer();
+    uiRenderer->material = om.newMaterial(uiStandard, "DieScreenMat", RenderType::Transparent);
+    uiRenderer->material->setVec4("color", {1.0f, 1.0f, 1.0f, 0.8f});
+    uiRenderer->material->setTexture("sprite", bg);
+
+    youDiedScreenEntity->addComponent(uiRenderer);
+
+    auto rt = new RectTransform();
+    rt->getAnchorModifiable() = {0.5f, 0.5f};
+    rt->getSizeModifiable() = {350, 400};
+    GetCore().uiModule.rootNodes.push_back(rt);
+    youDiedScreenEntity->addComponent(rt);
+
+    {
+        auto josefin = GetCore().objectModule.newFont("Resources/Fonts/JosefinSans-Regular.ttf", 128, "JosefinSans-Regular");
+        auto courier = GetCore().objectModule.newFont("Resources/Fonts/CourierNew.ttf", 128, "CourierNew");
+        auto textShader = GetCore().objectModule.newShader("TextStandard", "Resources/Shaders/TextStandard/TextStandard.vert", "Resources/Shaders/TextStandard/TextStandard.frag");
+        {
+            auto pauseText = new Entity(-9, 2, "youDiedText");
+            auto crt = new RectTransform();
+                crt->getSizeModifiable() = {0.5f, 0.5f};
+                crt->getLocalPositionModifiable() = {-152.0f, 128.0f};
+                crt->setParent(youDiedScreenEntity->getComponentPtr<RectTransform>());
+                pauseText->addComponent(crt);
+
+            auto cui = new TextRenderer();
+                cui->material = GetCore().objectModule.newMaterial(textShader, "pauseText", RenderType::Transparent);
+                cui->material->setVec4("color", {0.0f, 0.0f, 0.0f, 1.0f});
+                cui->mesh.font = courier;
+                cui->mesh.text = "You Died";
+                pauseText->addComponent(cui);
+        }
+
+        auto rect_border = om.newTexture("Resources/Sprites/rect_border.png", info);
+        glm::vec4 baseColor = {0.733f, 0.066f, 0.066f, 1.0f};
+        glm::vec4 inactiveColor = {0.733f, 0.066f, 0.066f, 1.0f};
+        glm::vec4 highlightedColor = {0.858f, 0.078f, 0.078f, 1.0f};
+        glm::vec4 onClickColor = {0.654f, 0.066f, 0.066f, 1.0f};
+
+        auto textMaterial = om.newMaterial(textShader, "buttonsText", RenderType::Transparent);
+        textMaterial->setVec4("color", baseColor);
+        {
+            auto entity = new Entity(-10, 3, "ToMenuButton");
+            auto crt = new RectTransform();
+                crt->getLocalPositionModifiable() = {2, 24};
+                crt->getSizeModifiable() = {250.0f, 100.0f};
+                crt->getLocalRotationModifiable() = glm::radians(0.8f);
+                crt->setParent(rt);
+                entity->addComponent(crt);
+
+            auto ui = new UiRenderer();
+                auto buttMaterial = om.newMaterial(uiStandard, "ToMenuButtonMat", RenderType::Transparent);
+                buttMaterial->setTexture("sprite", rect_border);
+                buttMaterial->setVec4("color", {1.0f, 0.0f, 1.0f, 0.5f});
+                ui->material = buttMaterial;
+                entity->addComponent(ui);
+            
+            auto butt = new Button();
+                butt->baseColor = baseColor;
+                butt->inactiveColor = inactiveColor;
+                butt->highlightedColor = highlightedColor;
+                butt->onClickColor = onClickColor;
+                butt->onClickEvents.push_back(Message(Event::PAUSE_GAME));
+                butt->onClickEvents.push_back(Message(Event::LOAD_SCENE, Scenes::mainMenuScene));
+                butt->isActive = true;
+                entity->addComponent(butt);
+        }
+
+        {
+            auto entity = new Entity(-11, 2, "ToMenuButtonText");
+            auto crt = new RectTransform();
+                crt->getLocalPositionModifiable() = {-113, 16};
+                crt->getSizeModifiable() = {0.25f, 0.25f};
+                crt->getLocalRotationModifiable() = glm::radians(0.8f);
+                crt->setParent(rt);
+                entity->addComponent(crt);
+
+            auto cui = new TextRenderer();
+                cui->material = textMaterial;
+                cui->mesh.font = courier;
+                cui->mesh.text = "Exit to menu";
+                entity->addComponent(cui);
+        }
+
+        {
+            auto entity = new Entity(-12, 3, "Exit_game");
+            auto crt = new RectTransform();
+                crt->getLocalPositionModifiable() = {2, -110};
+                crt->getSizeModifiable() = {250.0f, 100.0f};
+                crt->getLocalRotationModifiable() = glm::radians(-0.8f);
+                crt->setParent(rt);
+                entity->addComponent(crt);
+
+            auto ui = new UiRenderer();
+                auto buttMaterial = om.newMaterial(uiStandard, "Exit_gameMat", RenderType::Transparent);
+                buttMaterial->setTexture("sprite", rect_border);
+                buttMaterial->setVec4("color", {1.0f, 0.0f, 1.0f, 0.5f});
+                ui->material = buttMaterial;
+                entity->addComponent(ui);
+            
+            auto butt = new Button();
+                butt->baseColor = baseColor;
+                butt->inactiveColor = inactiveColor;
+                butt->highlightedColor = highlightedColor;
+                butt->onClickColor = onClickColor;
+                butt->onClickEvents.push_back(Message(Event::EXIT_GAME));
+                entity->addComponent(butt);
+        }
+
+        {
+            auto entity = new Entity(-13, 2, "Exit_game_text");
+            auto crt = new RectTransform();
+                crt->getLocalPositionModifiable() = {-88.0f, -119.0f};
+                crt->getSizeModifiable() = {0.25f, 0.25f};
+                crt->getLocalRotationModifiable() = glm::radians(-0.8f);
+                crt->setParent(rt);
+                entity->addComponent(crt);
+
+            auto cui = new TextRenderer();
+                cui->material = textMaterial;
+                cui->mesh.font = courier;
+                cui->mesh.text = "Exit Game";
+                entity->addComponent(cui);
+        }
+    }  
+}
+
 void GamePlayModule::pauseGame()
 {
-    Core::instance->gamePaused = !Core::instance->gamePaused;
-    Core::instance->messageBus.sendMessage(Message(Event::AUDIO_SOURCE_PAUSE_ALL_PLAYING));
     if(pauseScreenEntity != nullptr)
     {
-
         if(Core::instance->gamePaused)
         {
             GetCore().objectModule.objectContainer.entities.push_back(*pauseScreenEntity);
@@ -255,3 +398,40 @@ void GamePlayModule::pauseGame()
         }
     }
 }   
+
+void GamePlayModule::youDied()
+{
+    if(youDiedScreenEntity != nullptr)
+    {
+        if(Core::instance->gamePaused)
+        {
+            GetCore().objectModule.objectContainer.entities.push_back(*youDiedScreenEntity);
+            if(youDiedScreenEntity->getComponentPtr<RectTransform>() != nullptr)
+            {
+                for(auto pChildren : youDiedScreenEntity->getComponentPtr<RectTransform>()->children)
+                {
+                    GetCore().objectModule.objectContainer.entities.push_back(*pChildren->entityPtr);
+                }
+            }
+        }
+        else
+        {
+            auto iter2 = std::find(GetCore().objectModule.objectContainer.entities.begin(), GetCore().objectModule.objectContainer.entities.end(), *youDiedScreenEntity);
+            if(iter2 != GetCore().objectModule.objectContainer.entities.end())
+            {
+                GetCore().objectModule.objectContainer.entities.erase(iter2);
+            }
+            if(youDiedScreenEntity->getComponentPtr<RectTransform>() != nullptr)
+            {
+                for(auto pChildren : youDiedScreenEntity->getComponentPtr<RectTransform>()->children)
+                {
+                    auto iters = std::find(GetCore().objectModule.objectContainer.entities.begin(), GetCore().objectModule.objectContainer.entities.end(), *pChildren->entityPtr);
+                    if(iters != GetCore().objectModule.objectContainer.entities.end())
+                    {
+                        GetCore().objectModule.objectContainer.entities.erase(iters);
+                    }
+                }
+            }
+        }
+    }
+}
